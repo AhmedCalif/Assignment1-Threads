9f02c6232ef19ba36617970cd288dd1eb9f20185
ahmedcalif88 <ahmedcalif88@gmail.com>
2024-09-10 21:16:57 -0700

    fix: fixed a margining UI problem

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,7 +3,7 @@ import * as fakeDb from '@/fakeDb';
 import { type Post, type User } from "@/fakeDb";
 
 export default function Profile() {
-    const username = 'ahmedcalif'; // Change to desired username
+    const username = 'ahmedcalif'; 
     const getUser = fakeDb.getUser(username);
 
     return (
@@ -11,19 +11,19 @@ export default function Profile() {
             <NavBar />
                 {getUser ? (
                     <div className="flex flex-col items-center mt-24">
-                        {/* Avatar */}
+                       
                         <img
                             className="rounded-full h-24 w-24 object-cover mb-4"
                             src={getUser.avatar}
                             alt={`${getUser.firstName}'s avatar`}
                         />
 
-                        {/* User Information */}
+                       
                         <h1 className="text-3xl font-bold">{getUser.firstName} {getUser.lastName}</h1>
                         <h2 className="text-lg text-gray-300">{getUser.username}</h2>
                         <p className="text-gray-400 mt-2">{getUser.followers} followers</p>
 
-                        {/* Posts Section */}
+                       
                         <div className="mt-8 w-full px-8 max-w-4xl">
                             <h3 className="text-xl font-semibold border-b border-gray-600 pb-2">Posts</h3>
                             <ul>
